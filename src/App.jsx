9d4a261d294ed1b8afe2f4b2e7a1bf0ae1cb99d0
nawud <duwan17@gmail.com>
2024-10-21 20:47:28 +0200

    soundwave cambios

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Page from './components/Page/Page'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <Buttom title="Botón 1" text="Click botón 1"/>
+    <Buttom title="Botón 2" text="Click botón 2"/>
+    <Buttom title="Botón 3" text="Click botón 3"/> 
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,10 @@
+import Page from './components/Page/Page'
+function Page({title}) {
+  return (
+    <>
+    <h1>{title} hola </h1>
+    </>
+  )
+}
+
+export default Page
