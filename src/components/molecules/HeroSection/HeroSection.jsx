a4d7d1d9dbe1d04dd961e3edefea92b9521705ad
nawud <duwan17@gmail.com>
2024-10-24 14:15:35 +0200

    actualizacion de imagen

--- src/components/molecules/HeroSection/HeroSection.jsx
+++ src/components/molecules/HeroSection/HeroSection.jsx
@@ -1,17 +1,18 @@
-/* import './HeroSection.css';
-import img from '../../assets/images/landing-page-girl.png'
+import './HeroSection.css';
+import img from '../../../assets/images/landing-page-girl.png'
 
 function HeroSection() {
   return (
     <section className="hero-section">
+       <img src={img} alt="landing" className="hero-image" />
       <div className="hero-content">
         <h1 className="hero-title">Feel The Music</h1>
         <p className="hero-text">Stream over 20 thousand songs with one click</p>
         <button className="hero-button">Join Now</button>
       </div>
-      <img src={img} alt="landing" className="hero-image" />
+      <div className='background-template'></div>
     </section>
   );
 }
 
-export default HeroSection; */
+export default HeroSection; 
