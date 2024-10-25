ecbd131c1fb8efbe07f348083eb998c9ea5f9ee3
nawud <duwan17@gmail.com>
2024-10-25 14:00:49 +0200

    elementos añadidos

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <>
- 
-    <Header/>
-     <HeroSection/> 
+
+      <Header />
+      <HeroSection />
 
 
 
--- src/components/molecules/HeroSection/HeroSection.jsx
+++ src/components/molecules/HeroSection/HeroSection.jsx
@@ -3,15 +3,20 @@ import img from '../../../assets/images/landing-page-girl.png'
 
 function HeroSection() {
   return (
-    <section className="hero-section">
-       <img src={img} alt="landing" className="hero-image" />
-      <div className="hero-content">
-        <h1 className="hero-title">Feel The Music</h1>
-        <p className="hero-text">Stream over 20 thousand songs with one click</p>
-        <button className="hero-button">Join Now</button>
-      </div>
-      <div className='background-template'></div>
-    </section>
+
+    <div className="container">
+      <section className="hero-section">
+        <img src={img} alt="landing" className="hero-image" />
+        <div className="hero-content">
+          <h1 className="hero-title">Feel The Music</h1>
+          <p className="hero-text">Stream over 20 thousand songs with one click</p>
+          <button className="hero-button">Join Now</button>
+        </div>
+        <div className='background-template'></div>
+        <div className='background-template-2'></div>
+        <div className='background-template-3'></div>
+      </section>
+    </div>
   );
 }
 
